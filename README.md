4208145bf87a311abf96b4814bf1c6fa3270b321
cxmhfut <912750125@qq.com>
2018-07-09 11:15:04 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # LeoDemo
 
 App for Leo robot about dialogue system
+
+## Leo 机器人App安装到机器人内部步骤
+
+- 1 要求第三方程序要有Root权限才能使用，所以开发好的程序需要push到/system/app中才能正常使用。
+连接好机器人，打开命令窗口，输入adb shell，如果前面操作都没有问题的话应该出现如下提示：
+
+
--- app/src/main/java/com/hfut/leodemo/MainActivity.java
+++ app/src/main/java/com/hfut/leodemo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.hfut.leodemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.hfut.leodemo.Listener.MyIResultProcessor;
 import com.hfut.leodemo.Listener.MyIRobotListener;
@@ -9,9 +12,13 @@ import com.leo.api.LeoRobot;
 import com.leo.api.LeoSpeech;
 import com.leo.api.version.VersionManager;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.btn_base_demo)
+    Button btnBaseDemo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,13 @@ public class MainActivity extends AppCompatActivity {
         //设置机器人版本
         VersionManager.setVersion(VersionManager.ONE);
         initLeo();//初始化Leo机器人
+
+        btnBaseDemo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToBaseDemo();
+            }
+        });
     }
 
     /**
@@ -31,7 +45,15 @@ public class MainActivity extends AppCompatActivity {
         //初始化动作库服务 MyIRobotListener:动作执行回调监听类
         LeoRobot.init(this, new MyIRobotListener());
         //初始化语音服务 MyIResultProcessor:语音执行回调监听类
-        LeoSpeech.init(this,new MyIResultProcessor());
+        LeoSpeech.init(this, new MyIResultProcessor());
+    }
+
+    /**
+     * 跳转到BaseDemoActivity
+     */
+    public void goToBaseDemo() {
+        Intent intent = new Intent(MainActivity.this, BaseDemoActivity.class);
+        startActivity(intent);
     }
 
     @Override
